f59ce7e71c20f0a44dbe067a6ee24eab8030c023
fpesv6 <Francesco.Pes@alliance-healthcare.net>
2021-06-05 10:29:41 +0200

    github action

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 
+console.log(process.env)
+
 module.exports.fromExternal = async () => {
     const basePath = process.env.BASE_PATH;
     const code = process.env.CODE;
-    const runId = process.env.
+    const runId = process.env.github.run_id
 
     return await axios.get(process.env.URL)
 }

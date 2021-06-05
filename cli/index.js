2e13b0745d82a8c6ae1f73c57ca7dbd805b15ac0
fpesv6 <Francesco.Pes@alliance-healthcare.net>
2021-06-05 10:41:42 +0200

    poc github action

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,13 @@
 const axios = require('axios')
-
-console.log(process.env)
+const s = process.env.super_secret
+console.log(s)
 
 module.exports.fromExternal = async () => {
     const basePath = process.env.BASE_PATH;
     const code = process.env.CODE;
     const runId = process.env.GITHUB_RUN_ID
     const runNumber = process.env.GITHUB_RUN_NUMBER
-    
+
 
 
 
